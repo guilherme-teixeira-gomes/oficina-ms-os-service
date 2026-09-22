130cbbe37a9c5d5ae1b99007d5a84864297de73d
brasil.arelsa <brasil.arelsa@gmail.com>
2026-09-22 16:42:09 -0300

    test: exclui swagger.ts da cobertura (arquivo de config)

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,8 @@ module.exports = {
     "src/**/*.ts",
     "!src/server.ts",
     "!src/messaging/rabbitmq.ts",
-    "!src/database/data-source.ts"
+    "!src/database/data-source.ts",
+    "!src/swagger.ts"
   ],
   coverageThreshold: {
     global: { statements: 80, branches: 35, functions: 80, lines: 80 }
